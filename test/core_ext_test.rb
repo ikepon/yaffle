66d047047f24eef8f357b412c21d305e6a8057eb
ikepon <tatsuyanoheya@gmail.com>
2017-10-22 22:34:15 +0900

    to_squawk メソッドの追加

---
+++ test/core_ext_test.rb
@@ -0,0 +1,10 @@
+
+# yaffle/test/core_ext_test.rb
+
+require 'test_helper'
+
+class CoreExtTest < ActiveSupport::TestCase
+  def test_to_squawk_prepends_the_word_squawk
+    assert_equal "squawk! Hello World", "Hello World".to_squawk
+  end
+end
